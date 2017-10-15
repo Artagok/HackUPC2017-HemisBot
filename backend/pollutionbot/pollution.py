8ffc0929588779d075dd8671dbfa46157488b33b
ignasioliver <ignasioliverf@gmail.com>
2017-10-15 03:52:27 +0200

    Try to receive location directly from the first handle process

--- backend/pollutionbot/pollution.py
+++ backend/pollutionbot/pollution.py
@@ -48,34 +48,34 @@ def get_last_update_id(updates):
 def handle_updates(updates):
     for update in updates["result"]:
         try:
-            if True:
-            #if "location" in update:
-                chat = update["message"]["chat"]["id"]
-                send_message(update, chat)
-            #else:
-                received_text = update["message"]["text"]
-                chat = update["message"]["chat"]["id"]
-                # start the analysis:
-                if received_text == "/start":
-                    send_message("What up my boy! Tell me whatever...", chat)
-                elif received_text == "locate me":
-                    send_location(chat, 41.3880040, 2.1132800, reply_markup=None)
+            received_location = update["result"]["message"]["location"]["latitude"]
+            chat = update["message"]["chat"]["id"]
+            send_message("Thats a location man...", chat)
+
+
+            received_text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            # start the analysis:
+            if received_text == "/start":
+                send_message("What up my boy! Tell me whatever...", chat)
+            elif received_text == "locate me":
+                send_location(chat, 41.3880040, 2.1132800, reply_markup=None)
+            else:
+                records = db.get_records(chat)
+                if received_text in records:
+                    db.delete_record(received_text, chat)
+                    send_message("Record deleted!", chat)
+                elif received_text == "Nil":
+                    send_message("lol this man doesn't even like coffe", chat)
+                elif received_text == "Pau":
+                    send_message("yoh this man can't sleep on the floor", chat)
                 else:
+                    tosend_text = "You told me " + received_text + ". I'll save that. Your current list is (repeat an record to delete it):"
+                    send_message(tosend_text, chat)
+                    db.add_record(received_text, chat)
                     records = db.get_records(chat)
-                    if received_text in records:
-                        db.delete_record(received_text, chat)
-                        send_message("Record deleted!", chat)
-                    elif received_text == "Nil":
-                        send_message("lol this man doesn't even like coffe", chat)
-                    elif received_text == "Pau":
-                        send_message("yoh this man can't sleep on the floor", chat)
-                    else:
-                        tosend_text = "You told me " + received_text + ". I'll save that. Your current list is (repeat an record to delete it):"
-                        send_message(tosend_text, chat)
-                        db.add_record(received_text, chat)
-                        records = db.get_records(chat)
-                        all_records = "\n".join(records)
-                        send_message(all_records, chat)
+                    all_records = "\n".join(records)
+                    send_message(all_records, chat)
         except KeyError: # usually at the start of the conversation
             pass
 
